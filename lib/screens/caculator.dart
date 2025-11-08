0f97ec816726ba1bca42e0295a78564a4e31e280
Fábio Valentim <maitabom@gmail.com>
2025-11-08 20:15:41 -0300

    altera CalculatorScreen para StatefulWidget e integra a classe Memory para gerenciar o estado

--- lib/screens/caculator.dart
+++ lib/screens/caculator.dart
@@ -1,13 +1,33 @@
 import 'package:calculadora/components/display.dart';
+import 'package:calculadora/components/keyboard.dart';
+import 'package:calculadora/models/memory.dart';
 import 'package:flutter/material.dart';
 
-class CalculatorScreen extends StatelessWidget {
+class CalculatorScreen extends StatefulWidget {
   const CalculatorScreen({super.key});
+
+  @override
+  State<CalculatorScreen> createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  final Memory memory = Memory();
+
+  _onPressed(String text) {
+    setState(() {
+      memory.applyCommand(text);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Column(children: [Display('123,45'), Text('Keyboard')]),
+      home: Column(
+        children: [
+          Display(memory.value),
+          Keyboard(callback: _onPressed),
+        ],
+      ),
     );
   }
 }
