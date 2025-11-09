c5a3023e9389a308404b260aeecb7ca019cfa3d2
Fábio Valentim <maitabom@gmail.com>
2025-11-09 20:12:09 -0300

    refatora a classe Memory para melhorar a lógica de operações e gerenciamento de estado

--- lib/models/memory.dart
+++ lib/models/memory.dart
@@ -1,21 +1,103 @@
 class Memory {
+  static const operations = ['±', '%', '÷', 'X', '-', '+', '='];
+
+  String? _operation;
+  String? _lastCommand;
+
   String _value = "0";
+  final _buffer = [0.0, 0.0];
+  int _bufferIndex = 0;
+  bool _wipeValue = false;
 
   String get value {
     return _value;
   }
 
-  _allClear() {
-    _value = "0";
+  void applyCommand(String command) {
+    if (_isReplacingOperation(command)) {
+      _operation = command;
+      return;
+    }
+
+    if (command == "AC") {
+      _allClear();
+    } else if (operations.contains(command)) {
+      _setOperation(command);
+    } else {
+      _addDigit(command);
+    }
+
+    _lastCommand = command;
   }
 
-  void applyCommand(String command) {
-    switch (command) {
-      case "AC":
-        _allClear();
-        break;
-      default:
-        _value += command;
+  _allClear() {
+    _value = "0";
+    _buffer.setAll(0, [0.0, 0.0]);
+    _operation = null;
+    _bufferIndex = 0;
+    _wipeValue = false;
+  }
+
+  _setOperation(String newOperation) {
+    bool isEqualSign = newOperation == '=';
+
+    if (_bufferIndex == 0) {
+      if (!isEqualSign) {
+        _operation = newOperation;
+        _bufferIndex = 1;
+        _wipeValue = true;
+      }
+    } else {
+      _buffer[0] = _calculate();
+      _buffer[1] = 0.0;
+      _value = _buffer[0] % 1 == 0
+          ? _buffer[0].toStringAsFixed(0)
+          : _buffer[0].toString().replaceAll('.', ',');
+
+      _operation = isEqualSign ? null : newOperation;
+      _bufferIndex = isEqualSign ? 0 : 1;
+      _wipeValue = !isEqualSign;
     }
+  }
+
+  _addDigit(String digit) {
+    final isDot = (digit == ',');
+    final wipeValue = (_value == '0' && !isDot) || _wipeValue;
+
+    if (isDot && _value.contains('.') && !wipeValue) return;
+
+    final emptyValue = isDot ? '0' : '';
+    final currentValue = wipeValue ? emptyValue : _value;
+
+    _value = currentValue + digit;
+    _wipeValue = false;
+
+    _buffer[_bufferIndex] = double.tryParse(_value) ?? 0;
+  }
+
+  double _calculate() {
+    switch (_operation) {
+      case '±':
+        return _buffer[0] * -1;
+      case '%':
+        return _buffer[0] % _buffer[1];
+      case '÷':
+        return _buffer[1] != 0 ? _buffer[0] / _buffer[1] : 0;
+      case 'X':
+        return _buffer[0] * _buffer[1];
+      case '-':
+        return _buffer[0] - _buffer[1];
+      case '+':
+        return _buffer[0] + _buffer[1];
+      default:
+        return _buffer[0];
+    }
+  }
+
+  _isReplacingOperation(String command) {
+    return operations.contains(_lastCommand) &&
+        operations.contains(command) &&
+        _lastCommand != '=' &&
+        command != '=';
   }
 }
